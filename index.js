4ea25dfee9fc9d5f60870b88c432cea9661bca3e
BOULET Pierre <pierre.boulet@maif.fr>
2026-02-04 13:55:26 +0100

    feat(main): api call to display movies ok

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-fetch('./env')
+fetch('.env')
     .then(response => response.text()) 
     .then(response => response.split('=').pop())
-    .then(api_key => {console.log("API Key : ", api_key)})
     .then(main)
 
 async function main(api_key) {
-    
+    const sectionEl = document.querySelector(".movies");
+
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`)
+        .then(response => response.json())
+        .then(response => console.log(response))
+
 }
+
