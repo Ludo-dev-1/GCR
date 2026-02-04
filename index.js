8e92e7f7762f9f1c8db87ac61f3e2a054e49dd3f
BOULET Pierre <pierre.boulet@maif.fr>
2026-02-04 15:57:42 +0100

    feat(homepage): displaying title, poster and date of release ok

--- index.js
+++ index.js
@@ -6,9 +6,35 @@ fetch('.env')
 async function main(api_key) {
     const sectionEl = document.querySelector(".movies");
 
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`)
-        .then(response => response.json())
-        .then(response => console.log(response))
+    const upcomingMovies = await getUpcomingMovies(api_key);
+    // console.log(upcomingMovies);
 
+    for (const upcomingMovie of upcomingMovies) {
+        let titleEl = document.createElement("h3");
+        titleEl.textContent = upcomingMovie.original_title;
+        sectionEl.appendChild(titleEl);
+
+        const imageEl = document.createElement("img");
+        imageEl.src = "https://image.tmdb.org/t/p/w300/" + upcomingMovie.poster_path;
+        sectionEl.appendChild(imageEl);
+        
+        const releaseDateEl = document.createElement("p");
+        releaseDateEl.textContent = upcomingMovie.release_date;
+        sectionEl.appendChild(releaseDateEl);
+
+
+        
+    }
+
+/* TITRE DU FILM / AFFICHE / DATE DE SORTIE / DUREE / GENRE */
+/* original_title / poster_path / release_date / passage par l'id ? */
 }
 
+async function getUpcomingMovies(api_key) {
+
+    const upcomingMovies = fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}`)
+        .then(response => response.json())
+        .then(response => response.results);  
+
+    return upcomingMovies;
+}
