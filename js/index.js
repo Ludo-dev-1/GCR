4381464febed81e5d9d64501b941b165af79637a
BOULET Pierre <pierre.boulet@maif.fr>
2026-02-06 17:09:49 +0100

    feat(CSS & JS):adjusted one API call, order of data display + add CSS rules

--- js/index.js
+++ js/index.js
@@ -8,62 +8,46 @@ async function main(api_key) {
 
     for (const upcomingMovie of upcomingMovies) {
         const articleEl = document.createElement("article");
+        articleEl.style.backgroundImage = `url('https://image.tmdb.org/t/p/w500/${upcomingMovie.poster_path}')`;
         sectionEl.appendChild(articleEl);
+
+        let linkEl = document.createElement("a");
+        const movieId = upcomingMovie.id;
+        linkEl.href = `movie.html?movie=${movieId}`;
+        articleEl.appendChild(linkEl);
 
         let titleEl = document.createElement("h3");
         titleEl.textContent = upcomingMovie.original_title;
-        articleEl.appendChild(titleEl);
-
-        let linkEl = document.createElement("a");
-        
-        const movieId = upcomingMovie.id;
-
-        linkEl.href = `movie.html?movie=${movieId}`;
-
-        const imageEl = document.createElement("img");
-        imageEl.src = "https://image.tmdb.org/t/p/w300/" + upcomingMovie.poster_path;
-        linkEl.appendChild(imageEl);
-
-        articleEl.appendChild(linkEl);
+        linkEl.appendChild(titleEl);
         
         const releaseDateEl = document.createElement("p");
         releaseDateEl.textContent = upcomingMovie.release_date;
-        articleEl.appendChild(releaseDateEl);
-
+        linkEl.appendChild(releaseDateEl);
 
         const movieDetails = await getDetailsMovie(api_key, movieId);
 
+        const movieRuntimeEl = document.createElement("p");
+        movieRuntimeEl.textContent = "Durée : " + movieDetails.runtime + " minutes";
+        linkEl.appendChild(movieRuntimeEl);
+
         const movieGenresParentEl = document.createElement("ul");
-        articleEl.appendChild(movieGenresParentEl);
+        linkEl.appendChild(movieGenresParentEl);
         
         for (const genre of movieDetails.genres) {
             const movieGenreEl = document.createElement("li");
             movieGenreEl.textContent = genre.name;
             movieGenresParentEl.appendChild(movieGenreEl);
-        }
+        };
 
-        const movieRuntimeEl = document.createElement("p");
-        movieRuntimeEl.textContent = "Durée : " + movieDetails.runtime + " minutes";
-        articleEl.appendChild(movieRuntimeEl);        
-    }
-}
+    };
+};
 
 async function getUpcomingMovies(api_key) {
 
     const upcomingMovies = 
-        // fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}&language=fr&page=1&region=fr&language=fr`)
-        //fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&include_adult=false&include_video=false&language=fr-FR&page=1&primary_release_date.gte=2026-02-04&region=fr-FR&sort_by=primary_release_date.asc&with_runtime.gte=60`)
-        fetch(`https://api.themoviedb.org/3/discover/movie
-  ?api_key=YOUR_API_KEY
-  &language=fr-FR
-  &region=FR
-  &sort_by=release_date.asc
-  &with_runtime.gte=60
-  &release_date.gte=2026-02-06
-`)
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_release_type=3&include_adult=false&include_video=false&language=fr-FR&page=1&region=FR&release_date.gte=2026-02-04&sort_by=release_date.asc&with_runtime.gte=60`)
         .then(response => response.json())
         .then(response => response.results);  
 
     return upcomingMovies;
-}
-
+};
