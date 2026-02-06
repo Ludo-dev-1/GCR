bf71a14e2179e99fa9af9188b0960154a3bac855
Ludovic Thibaud <164179276+Ludo-dev-1@users.noreply.github.com>
2026-02-06 14:00:59 +0100

    chore: add css to movie page and new fetch in french (#3)
    Co-authored-by: THIBAUD Ludovic <ludovic.thibaud@maif.fr>

--- js/commun.js
+++ js/commun.js
@@ -1,9 +1,13 @@
 async function getDetailsMovie(api_key, movieId) {
-    const movieDetails = fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${api_key}`)
-        .then(response => response.json());
-    
-    return movieDetails; 
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${movieId}?api_key=${api_key}&language=fr-FR`
+    );
+
+    const movieDetails = await response.json();
+    return movieDetails;
 }
+
+
 
 async function getApiKey() {
   return fetch('.env')
